2fe33ed22356a553c0c9cfdf9993ab487d7cfb2a
Valtteri Verronen <valtteriverronen@gmail.com>
2024-02-08 19:39:22 +0200

    Working toggle

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
   const colorWheel = document.getElementById("color-wheel");
   let isMouseDown = false;
+  let isLightOn = false;
+  // used for when light is switched off and then on
+  let lastColor = "green";
 
   const setBodyColor = (e) => {
+    if (!isLightOn) return;
+
     const rect = colorWheel.getBoundingClientRect();
     const x = e.clientX - rect.left - colorWheel.clientWidth / 2;
     const y = e.clientY - rect.top - colorWheel.clientHeight / 2;
@@ -16,19 +21,20 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const color = `hsl(${degrees}, 100%, 50%)`;
     document.body.style.backgroundColor = color;
+    lastColor = color;
   };
 
-  colorWheel.addEventListener("click", function (event) {
+  colorWheel.addEventListener("click", (event) => {
     setBodyColor(event);
   });
 
-  colorWheel.addEventListener("mousemove", function (event) {
+  colorWheel.addEventListener("mousemove", (event) => {
     if (isMouseDown) {
       setBodyColor(event);
     }
   });
 
-  colorWheel.addEventListener("touchmove", function (event) {
+  colorWheel.addEventListener("touchmove", (event) => {
     // Without this browser tries to scroll
     event.preventDefault();
 
@@ -36,14 +42,36 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   // for pc use, update the left mouse click state
-  colorWheel.addEventListener("mousedown", function (event) {
+  colorWheel.addEventListener("mousedown", (event) => {
     // 0 is left mouse click
     if (event.button === 0) {
       isMouseDown = true;
     }
   });
 
-  colorWheel.addEventListener("mouseup", function () {
+  colorWheel.addEventListener("mouseup", () => {
     isMouseDown = false;
   });
+
+  /**
+   * Toggle logic
+   */
+  const lightToggle = document.getElementById("light-toggle");
+
+  lightToggle.addEventListener("click", (e) => {
+    e.stopPropagation();
+
+    isLightOn = !isLightOn;
+
+    if (!isLightOn) {
+      document.body.style.backgroundColor = "#000";
+      return;
+    }
+
+    document.body.style.backgroundColor = lastColor;
+  });
+
+  // Stop propagation from move events as well
+  lightToggle.addEventListener("mousemove", (e) => e.stopPropagation());
+  lightToggle.addEventListener("touchmove", (e) => e.stopPropagation());
 });
